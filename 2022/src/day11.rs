f7f4df873f8cdb67c473cc6509d0c6bad6e66418
Lukas Fischer <git@o1oo11oo.de>
2022-12-05 16:26:54 +0100

    Prepare files for all upcoming days

---
+++ 2022/src/day11.rs
@@ -0,0 +1,7 @@
+pub(crate) fn part_1(input: &str) -> String {
+    "Not implemented!".to_string()
+}
+
+pub(crate) fn part_2(input: &str) -> String {
+    "Not implemented!".to_string()
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -2,20 +2,60 @@
 
 use clap::Parser;
 
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
 
 type ProblemFns = (fn(&str) -> String, fn(&str) -> String);
 
 const DAYS: [ProblemFns; 5] = [
-    (day1::part_1, day1::part_2),
-    (day2::part_1, day2::part_2),
-    (day3::part_1, day3::part_2),
-    (day4::part_1, day4::part_2),
-    (day5::part_1, day5::part_2),
+    (day01::part_1, day01::part_2),
+    (day02::part_1, day02::part_2),
+    (day03::part_1, day03::part_2),
+    (day04::part_1, day04::part_2),
+    (day05::part_1, day05::part_2),
+    //(day06::part_1, day06::part_2),
+    //(day07::part_1, day07::part_2),
+    //(day08::part_1, day08::part_2),
+    //(day09::part_1, day09::part_2),
+    //(day10::part_1, day10::part_2),
+    //(day11::part_1, day11::part_2),
+    //(day12::part_1, day12::part_2),
+    //(day13::part_1, day13::part_2),
+    //(day14::part_1, day14::part_2),
+    //(day15::part_1, day15::part_2),
+    //(day16::part_1, day16::part_2),
+    //(day17::part_1, day17::part_2),
+    //(day18::part_1, day18::part_2),
+    //(day19::part_1, day19::part_2),
+    //(day20::part_1, day20::part_2),
+    //(day21::part_1, day21::part_2),
+    //(day22::part_1, day22::part_2),
+    //(day23::part_1, day23::part_2),
+    //(day24::part_1, day24::part_2),
+    //(day25::part_1, day25::part_2),
 ];
 
 #[derive(Parser, Debug)]
