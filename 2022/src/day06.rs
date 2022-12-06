150668945162eeaed3382b0c89f2f75956d58230
Lukas Fischer <git@o1oo11oo.de>
2022-12-06 14:49:52 +0100

    Day 6

--- 2022/src/day06.rs
+++ 2022/src/day06.rs
@@ -1,10 +1,30 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
+use std::collections::{hash_map::RandomState, HashSet};
 
 pub(crate) fn part_1(input: &str) -> String {
-    "Not implemented!".to_string()
+    let (count, _) = input
+        .as_bytes()
+        .windows(4)
+        .enumerate()
+        .find(|&(_, win)| {
+            win[0] != win[1]
+                && win[0] != win[2]
+                && win[0] != win[3]
+                && win[1] != win[2]
+                && win[1] != win[3]
+                && win[2] != win[3]
+        })
+        .unwrap();
+
+    (count + 4).to_string()
 }
 
 pub(crate) fn part_2(input: &str) -> String {
-    "Not implemented!".to_string()
+    let (count, _) = input
+        .as_bytes()
+        .windows(14)
+        .enumerate()
+        .find(|&(_, win)| HashSet::<_, RandomState>::from_iter(win).len() == 14)
+        .unwrap();
+
+    (count + 14).to_string()
 }
