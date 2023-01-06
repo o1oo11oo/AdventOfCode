4592ca79a73e3ebc6cbce244984ae9ad089d4918
Lukas Fischer <git@o1oo11oo.de>
2023-01-06 20:13:38 +0100

    2015 day 3

--- 2015/src/day03.rs
+++ 2015/src/day03.rs
@@ -1,10 +1,52 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
+use std::collections::HashSet;
 
 pub(crate) fn part_1(input: &str) -> String {
-    "Not implemented!".to_string()
+    input
+        .trim()
+        .as_bytes()
+        .iter()
+        .fold(((0, 0), HashSet::from([(0, 0)])), |mut acc, curr| {
+            let new = match curr {
+                b'^' => (acc.0 .0, acc.0 .1 - 1),
+                b'v' => (acc.0 .0, acc.0 .1 + 1),
+                b'<' => (acc.0 .0 - 1, acc.0 .1),
+                b'>' => (acc.0 .0 + 1, acc.0 .1),
+                _ => (acc.0 .0, acc.0 .1),
+            };
+            acc.1.insert(new);
+            (new, acc.1)
+        })
+        .1
+        .len()
+        .to_string()
 }
 
 pub(crate) fn part_2(input: &str) -> String {
-    "Not implemented!".to_string()
+    input
+        .trim()
+        .as_bytes()
+        .iter()
+        .enumerate()
+        .fold(
+            ((0, 0), (0, 0), HashSet::from([(0, 0)])),
+            |mut acc, curr| {
+                let pos = if curr.0 % 2 == 0 {
+                    &mut acc.0
+                } else {
+                    &mut acc.1
+                };
+                *pos = match curr.1 {
+                    b'^' => (pos.0, pos.1 - 1),
+                    b'v' => (pos.0, pos.1 + 1),
+                    b'<' => (pos.0 - 1, pos.1),
+                    b'>' => (pos.0 + 1, pos.1),
+                    _ => (pos.0, pos.1),
+                };
+                acc.2.insert(*pos);
+                acc
+            },
+        )
+        .2
+        .len()
+        .to_string()
 }
