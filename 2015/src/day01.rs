c528363184e084bc2c946d0f03e1aeec862c7098
Lukas Fischer <git@o1oo11oo.de>
2023-01-06 19:14:39 +0100

    2015 day 1

--- 2015/src/day01.rs
+++ 2015/src/day01.rs
@@ -1,10 +1,34 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-
 pub(crate) fn part_1(input: &str) -> String {
-    "Not implemented!".to_string()
+    input
+        .as_bytes()
+        .iter()
+        .map(|c| match c {
+            b'(' => 1,
+            b')' => -1,
+            _ => 0,
+        })
+        .sum::<i32>()
+        .to_string()
 }
 
 pub(crate) fn part_2(input: &str) -> String {
-    "Not implemented!".to_string()
+    input
+        .as_bytes()
+        .iter()
+        .map(|c| match c {
+            b'(' => 1,
+            b')' => -1,
+            _ => 0,
+        })
+        .enumerate()
+        .fold((None, 0), |mut acc, curr| {
+            acc.1 += curr.1;
+            if acc.1 == -1 && acc.0.is_none() {
+                acc.0 = Some(curr.0 + 1)
+            }
+            acc
+        })
+        .0
+        .unwrap()
+        .to_string()
 }
