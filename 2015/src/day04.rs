6f86c5a9bd0323e671e7424115a2498d5edae120
Lukas Fischer <git@o1oo11oo.de>
2023-01-07 02:03:41 +0100

    Improvements for 2015 day 4

--- 2015/src/day04.rs
+++ 2015/src/day04.rs
@@ -1,20 +1,30 @@
+use rayon::prelude::*;
+
 pub(crate) fn part_1(input: &str) -> String {
-    (0..)
-        .find(|num| {
-            let phrase = format!("{}{num}", input.trim());
-            let hash = md5::compute(phrase);
-            hash[0] == 0 && hash[1] == 0 && hash[2] & 0xf0 == 0
+    (0_u64..)
+        .step_by(250_000)
+        .find_map(|bound| {
+            (bound..bound + 250_000).into_par_iter().find_first(|num| {
+                let phrase = format!("{}{num}", input.trim());
+                let hash = md5::compute(phrase);
+                hash[0] == 0 && hash[1] == 0 && hash[2] & 0xf0 == 0
+            })
         })
         .unwrap()
         .to_string()
 }
 
 pub(crate) fn part_2(input: &str) -> String {
-    (0..)
-        .find(|num| {
-            let phrase = format!("{}{num}", input.trim());
-            let hash = md5::compute(phrase);
-            hash[0] == 0 && hash[1] == 0 && hash[2] == 0
+    (0_u64..)
+        .step_by(1_000_000)
+        .find_map(|bound| {
+            (bound..bound + 1_000_000)
+                .into_par_iter()
+                .find_first(|num| {
+                    let phrase = format!("{}{num}", input.trim());
+                    let hash = md5::compute(phrase);
+                    hash[0] == 0 && hash[1] == 0 && hash[2] == 0
+                })
         })
         .unwrap()
         .to_string()
