791d77ea02a31387cbaa039a932ad18d87a5f9f8
Lukas Fischer <git@o1oo11oo.de>
2023-01-09 01:54:55 +0100

    2015 day 10

--- 2015/src/day10.rs
+++ 2015/src/day10.rs
@@ -1,10 +1,53 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-
 pub(crate) fn part_1(input: &str) -> String {
-    "Not implemented!".to_string()
+    look_and_say_steps(
+        input
+            .trim()
+            .as_bytes()
+            .iter()
+            .map(|c| c - b'0')
+            .collect::<Vec<_>>(),
+        40,
+    )
+    .len()
+    .to_string()
 }
 
 pub(crate) fn part_2(input: &str) -> String {
-    "Not implemented!".to_string()
+    look_and_say_steps(
+        input
+            .trim()
+            .as_bytes()
+            .iter()
+            .map(|c| c - b'0')
+            .collect::<Vec<_>>(),
+        50,
+    )
+    .len()
+    .to_string()
+}
+
+fn look_and_say_steps(nums: Vec<u8>, steps: usize) -> Vec<u8> {
+    itertools::iterate(nums, look_and_say_step)
+        .skip(1)
+        .take(steps)
+        .last()
+        .unwrap()
+}
+
+fn look_and_say_step(nums: &Vec<u8>) -> Vec<u8> {
+    let mut new = Vec::with_capacity(nums.len() * 27 / 20);
+    let mut idx = 0;
+    while idx < nums.len() {
+        if idx + 2 < nums.len() && nums[idx] == nums[idx + 1] && nums[idx + 1] == nums[idx + 2] {
+            new.extend([3, nums[idx]]);
+            idx += 3;
+        } else if idx + 1 < nums.len() && nums[idx] == nums[idx + 1] {
+            new.extend([2, nums[idx]]);
+            idx += 2;
+        } else {
+            new.extend([1, nums[idx]]);
+            idx += 1;
+        }
+    }
+    new
 }
