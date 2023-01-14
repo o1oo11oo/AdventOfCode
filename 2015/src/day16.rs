c6ab97444a86ac346a6fe160ad6657ade975ab10
Lukas Fischer <git@o1oo11oo.de>
2023-01-14 01:44:45 +0100

    2015 day 16

--- 2015/src/day16.rs
+++ 2015/src/day16.rs
@@ -1,10 +1,90 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
+use std::collections::HashMap;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{alpha1, u16, u8},
+    multi::separated_list1,
+    sequence::{delimited, terminated, tuple},
+    Finish, IResult,
+};
 
 pub(crate) fn part_1(input: &str) -> String {
-    "Not implemented!".to_string()
+    let search = HashMap::from([
+        ("children", 3),
+        ("cats", 7),
+        ("samoyeds", 2),
+        ("pomeranians", 3),
+        ("akitas", 0),
+        ("vizslas", 0),
+        ("goldfish", 5),
+        ("trees", 3),
+        ("cars", 2),
+        ("perfumes", 1),
+    ]);
+
+    input
+        .lines()
+        .map(parse_sue)
+        .find_map(|(id, properties)| {
+            properties
+                .iter()
+                .all(|(prop_name, prop_val)| search.get(prop_name) == Some(prop_val))
+                .then_some(id)
+        })
+        .unwrap()
+        .to_string()
 }
 
 pub(crate) fn part_2(input: &str) -> String {
-    "Not implemented!".to_string()
+    use Compare::*;
+    let search = HashMap::from([
+        ("children", Equal(3)),
+        ("cats", Greater(7)),
+        ("samoyeds", Equal(2)),
+        ("pomeranians", Less(3)),
+        ("akitas", Equal(0)),
+        ("vizslas", Equal(0)),
+        ("goldfish", Less(5)),
+        ("trees", Greater(3)),
+        ("cars", Equal(2)),
+        ("perfumes", Equal(1)),
+    ]);
+
+    input
+        .lines()
+        .map(parse_sue)
+        .find_map(|(id, properties)| {
+            properties
+                .iter()
+                .all(|(prop_name, prop_val)| match search.get(prop_name) {
+                    Some(Less(val)) => prop_val < val,
+                    Some(Equal(val)) => prop_val == val,
+                    Some(Greater(val)) => prop_val > val,
+                    None => false,
+                })
+                .then_some(id)
+        })
+        .unwrap()
+        .to_string()
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Compare {
+    Less(u8),
+    Equal(u8),
+    Greater(u8),
+}
+
+fn parse_sue(input: &str) -> (u16, HashMap<&str, u8>) {
+    type ParseRes<'a> = (u16, Vec<(&'a str, u8)>);
+    fn parse(input: &str) -> IResult<&str, ParseRes> {
+        tuple((
+            delimited(tag("Sue "), u16, tag(": ")),
+            separated_list1(tag(", "), tuple((terminated(alpha1, tag(": ")), u8))),
+        ))(input)
+    }
+
+    let (id, values) = parse(input).finish().unwrap().1;
+    let properties = values.into_iter().collect::<HashMap<_, _>>();
+    (id, properties)
 }
--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -28,7 +28,7 @@ mod day25;
 
 type ProblemFns = (fn(&str) -> String, fn(&str) -> String);
 
-const DAYS: [ProblemFns; 15] = [
+const DAYS: [ProblemFns; 16] = [
     (day01::part_1, day01::part_2),
     (day02::part_1, day02::part_2),
     (day03::part_1, day03::part_2),
@@ -44,7 +44,7 @@ const DAYS: [ProblemFns; 15] = [
     (day13::part_1, day13::part_2),
     (day14::part_1, day14::part_2),
     (day15::part_1, day15::part_2),
-    //(day16::part_1, day16::part_2),
+    (day16::part_1, day16::part_2),
     //(day17::part_1, day17::part_2),
     //(day18::part_1, day18::part_2),
     //(day19::part_1, day19::part_2),
